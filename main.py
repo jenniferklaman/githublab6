0ac75ff27f5be5366b3620e6dc75d45255b7b5a1
jen <jenniferklaman@gmail.com>
2023-03-09 12:12:49 -0500

    hi

---
+++ main.py
@@ -0,0 +1,16 @@
+
+def main():
+    main()
+
+
+def print_menu():
+
+
+Please enter an option: 1
+Please enter your password to encode: 12345555
+Your password has been encoded and stored!
+
+
+if __name__ == "__main__":
+
+
