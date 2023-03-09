1f2a441015fe041a2aee2b888dc4e12c89237d81
jen <jenniferklaman@gmail.com>
2023-03-09 12:33:13 -0500

    hi

--- main.py
+++ main.py
@@ -4,13 +4,32 @@ def main():
 
 
 def print_menu():
+    print("Menu")
+    print("------------- ")
+    print("1. Encode")
+    print("2. Decode")
+    print("3. Quit")
 
-
-Please enter an option: 1
-Please enter your password to encode: 12345555
-Your password has been encoded and stored!
 
 
 if __name__ == "__main__":
+
+while True:
+    def print_menu():
+
+option = int(input("Please enter an option: "))
+
+if option == 1:
+    print("Please enter your password to encode: ")
+    def encode():
+
+    print("Your password has been encoded and stored!")
+    def print_menu():
+
+elif option == 2:
+    def decode():
+    print(f"The encoded password is {encoded}, and the original password is {decoded}")
+
+elif option == 3:
 
 
