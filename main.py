3e81836a55157af8c64a340e98bf078c0b229a34
ZachJordanUF <zachjordan@ufl.edu>
2023-03-22 14:13:53 -0400

    decode added

--- main.py
+++ main.py
@@ -1,23 +1,11 @@
-def decode(password):
-
-    password_lst = []
-    decoded_password = ""
-
-    try:
-        
-        for i in password:
-            password_lst.append(i)
-        
-        for digit in password_lst:
-            digit = int(digit)
-            digit -= 3
-            digit = str(digit)
-            decoded_password += digit
-
-        return decoded_password
-    
-    except TypeError as exc:
-        print(exc)
+def decode(encoded_pass):
+    ls_pass = list(encoded_pass)
+    dec_pass = ""
+    for i in (ls_pass):
+        updated_pass = int(i) - 3
+        string = str(updated_pass)
+        dec_pass += string
+    return dec_pass
 
 def main():
     main()
